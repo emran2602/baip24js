25a4cc7b1c0761834d04f09ff7dade6efadc5a40
emran2602 <emran.maj@gmail.com>
2024-02-12 15:42:52 -0800

    Update main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector("#chanterelle-button").addEventListener("click", function(){
+    document.querySelector("#header").innerHTML = ("chanterelle")
+    document.querySelector("#image").src = "chanterelle.jpg"
+    document.querySelector("#credit").innerHTML = "Neil Leclerc"
+})
+
+document.querySelector("#oyster-button").addEventListener("click", function(){
+    document.querySelector("#header").innerHTML = ("oyster")
+    document.querySelector("#image").src = "oyster.jpg"
+    document.querySelector("#credit").innerHTML = "Neil Leclerc"
+})
